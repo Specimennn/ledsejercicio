5702333d0aef6b467f98b814fae914911507f019
Alejandro Álvarez <118235591+Specimennn@users.noreply.github.com>
2023-05-09 16:20:45 -0500

    Delete ledsejercicio directory

--- ledsejercicio/src/main/java/co/edu/umanizales/ledsejercicio/exception/DLListException.java
+++
@@ -1,7 +0,0 @@
-package co.edu.umanizales.ledsejercicio.exception;
-
-public class DLListException extends Exception{
-    public DLListException(String message) {
-        super(message);
-    }
-}
